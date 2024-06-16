18fd81fd03c9da3aa8496680cdd03d5825f0f017
Constanza Pérez <constanza.vp.o@gmail.com>
2024-06-16 12:45:55 -0400

    operación leer

--- operaciones.js
+++ operaciones.js
@@ -1,1 +1,11 @@
-const fs = 
+const { Console } = require('console');
+const fs = require('fs');
+
+const leer = ()=>{
+    try {
+        const data = fs.readFileSync('citas.json' , 'utf-8')
+        console.log(data.toString())
+    } catch (error) {
+        console.log('No se puede leer el archivo')
+    }
+}
