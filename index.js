f7af18b0a993de8c91c400c8ffa7349863e480aa
Constanza Pérez <constanza.vp.o@gmail.com>
2024-06-16 13:16:52 -0400

    operación registrar

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-console.log(process.argv.slice(2))
+// console.log(process.argv.slice(2))
 // argv es el parametro que recive los datos que le pasamos por consola.
 // El slice ayuda a que se muestren los datos desde el indice 2
+
+const {leer, registrar} = require('./operaciones')
 
 const [operacion, nombre, edad, tipo, color, enfermedad] = process.argv.slice(2)
 console.log(operacion, nombre, edad, tipo, color, enfermedad)
 
 if(operacion.trim().toLowerCase() === "registrar"){
-    // registrar(nombre, edad, animal, color, enfermedad)
-    console.log("pasó el registro")
-    }
-else if(operacion.trim().toLowerCase() === "leer"){
-    // leer()
-    console.log("pasó leer")
-    }
-else {
+    let resultado = registrar(nombre, edad, tipo, color, enfermedad)
+    console.log(resultado, "pasó el registro")
+}else if(operacion.trim().toLowerCase() === "leer"){
+    let resultado = leer()
+    console.log(resultado, "pasó leer")
+}else {
     console.log("Ingrese la opción registrar o leer")
 }
--- operaciones.js
+++ operaciones.js
@@ -4,8 +4,21 @@ const fs = require('fs');
 const leer = ()=>{
     try {
         const data = fs.readFileSync('citas.json' , 'utf-8')
-        console.log(data.toString())
+        return data.toString()
     } catch (error) {
         console.log('No se puede leer el archivo')
     }
 }
+
+const registrar= (nombre, edad, tipo, color, enfermedad)=>{
+    try {
+        const data = leer()
+        const citas =JSON.parse(data)
+        citas.push({nombre, edad, tipo, color, enfermedad})
+        fs.writeFileSync('citas.json', JSON.stringify(citas))
+    } catch (error) {
+        console.log('No se pudo registrar el nuevo paciente')
+    }
+}
+
+module.exports={leer, registrar}
